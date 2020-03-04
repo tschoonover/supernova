7b016c0caf29f302fcb073e4dbd555942842fe8e
Timothy Schoonover <tschoonover@expediagroup.com>
2020-03-04 14:07:43 -0600

    Add logic to prevent multiple sounds from playing concurrently

--- supernova.py
+++ supernova.py
@@ -8,15 +8,9 @@ def main():
     
     sounds = [pygame.mixer.Sound(f) for f in glob.glob("sounds/*.wav")]
     
-    is_playing = False
-    
     def play_sound():
-        nonlocal is_playing
-        
-        if not is_playing:
-            is_playing = True
+        if not pygame.mixer.get_busy():
             sounds[randrange(len(sounds))].play()
-            is_playing = False
             
     button = Button(4, bounce_time = 0.1)
     button.when_pressed = play_sound
